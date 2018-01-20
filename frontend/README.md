350aef60fadef6169db8caa62b0689a0fdfd5ea0
fernandosouto <fernando.souto@mobgen.com>
2018-01-20 01:47:12 +0100

    Update frontend to support dynamic categories from api and prepare config file to update every year

--- frontend/README.md
+++ frontend/README.md
@@ -1,2 +1,3 @@
 # oscuacs
 Frontend to manage the CUAC FM Annual Awards
+You have to change year in config.js
---
+++ frontend/config/config.js
@@ -0,0 +1,14 @@
+function yearHTML() {
+	//replae the year
+    document.getElementById("year_tag").innerHTML = "2018";
+}
+
+function getApi(){
+	//base url
+	return "https://cuacfm.org/oscuacsapi/api/";
+}
+
+function getContest(){
+	//replace this contest id every year
+	return "OSCUACS_2018";
+}
